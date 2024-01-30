9a343efe8d42d5b641e1f1b2f117b3f88b47fbb4
telsamar <56135473+telsamar@users.noreply.github.com>
2024-01-31 02:55:13 +0300

    Update ElementsContainerComponent.jsx

--- reactjs/my-app/src/components/interface/body/ListComponent/ElementsContainerComponent.jsx
+++ reactjs/my-app/src/components/interface/body/ListComponent/ElementsContainerComponent.jsx
@@ -1,31 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function ElementsContainerComponent(props) {
-  const { urls } = props.state;
-  const [selectedUrlId, setSelectedUrlId] = useState(null);
+  const { urls, currentURL_ID } = props.state;
 
   const handleDivSelection = (id) => {
-    setSelectedUrlId(id);
     props.handleButtonClick(id);
   };
 
   return (
     <div style={{ maxHeight: '150px', overflowY: 'auto', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}>
       {urls.map((url) => (
-          <div 
-            key={url.id}
-            style={{
-              padding: '10px',
-              margin: '5px 0',
-              backgroundColor: selectedUrlId === url.id ? '#28a745' : '#f8f9fa',
-              color: selectedUrlId === url.id ? 'white' : 'black',
-              cursor: 'pointer',
-              borderRadius: '5px',
-              textAlign: 'center',
-              border: '1px solid black'
-            }} 
-            onClick={() => handleDivSelection(url.id)}
-          >
+        <div 
+          key={url.id}
+          style={{
+            padding: '10px',
+            margin: '5px 0',
+            backgroundColor: currentURL_ID === url.id ? '#28a745' : '#f8f9fa',
+            color: currentURL_ID === url.id ? 'white' : 'black',
+            cursor: 'pointer',
+            borderRadius: '5px',
+            textAlign: 'center',
+            border: '1px solid black'
+          }} 
+          onClick={() => handleDivSelection(url.id)}
+        >
           {url.name}
         </div>
       ))}
