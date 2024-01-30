00276043942f9707f1692449ed9df7ed8776b55c
telsamar <56135473+telsamar@users.noreply.github.com>
2024-01-30 23:58:34 +0300

    - Button

--- reactjs/my-app/src/components/interface/body/ListComponent/ElementsContainerComponent.jsx
+++ reactjs/my-app/src/components/interface/body/ListComponent/ElementsContainerComponent.jsx
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
 
 function ElementsContainerComponent(props) {
   const { urls } = props.state;
-  
   const [selectedUrlIndex, setSelectedUrlIndex] = useState(-1);
 
-  const handleButtonSelection = (index) => {
+  const handleDivSelection = (index) => {
     setSelectedUrlIndex(index);
     props.handleButtonClick(index);
   };
 
-  useEffect(() => {
-    console.log('Текущее состояние:', props.state);
-  }, [props.state]); 
-
   return (
-    <div style={{ maxHeight: '150px', overflowY: 'auto', border: '1px solid #ccc', padding: '10px', borderRadius: '5px' }} >
+    <div style={{ maxHeight: '150px', overflowY: 'auto', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}>
       {urls.map((url, index) => (
-        <Button key={index} variant={selectedUrlIndex === index ? 'success' : 'outline-secondary'} className="w-100 mb-2" onClick={() => handleButtonSelection(index)} >
+          <div 
+            key={index} 
+            style={{
+              padding: '10px',
+              margin: '5px 0',
+              backgroundColor: selectedUrlIndex === index ? '#28a745' : '#f8f9fa',
+              color: selectedUrlIndex === index ? 'white' : 'black',
+              cursor: 'pointer',
+              borderRadius: '5px',
+              textAlign: 'center',
+              border: '1px solid black'
+            }} 
+            onClick={() => handleDivSelection(index)}
+          >
           {url.name}
-        </Button>
+        </div>
       ))}
     </div>
   );
--- reactjs/my-app/src/components/main/MainComponent.jsx
+++ reactjs/my-app/src/components/main/MainComponent.jsx
@@ -5,10 +5,10 @@ import MenuComponent from '../interface/menu/MenuComponent';
 function MainComponent() {
   const [state, setState] = useState({
     urls: [
-      { name: "url_1", url: "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json" },
-      { name: "url_2", url: "https://filesamples.com/samples/code/json/sample4.json" },
-      { name: "url_3", url: "https://my-json-server.typicode.com/typicode/demo/db" },
-      { name: "url_4", url: "https://my-json-server.typicode.com/typicode/demo/comments" },
+      { id: 1, name: "url_1", url: "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json" },
+      { id: 2,name: "url_2", url: "https://filesamples.com/samples/code/json/sample4.json" },
+      { id: 3, name: "url_3", url: "https://my-json-server.typicode.com/typicode/demo/db" },
+      { id: 4, name: "url_4", url: "https://my-json-server.typicode.com/typicode/demo/comments" },
     ],
     loadedJSON: {},
     countRows: 0,
